1d3fb67af6eda56b529666f0f3f66d684dca1b13
AxonDesigns <acciol400@gmail.com>
2024-05-21 15:29:28 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# field_validators
+
+<img src="https://forthebadge.com/images/badges/built-with-love.svg" height="28px" />
+
+Easy to use validators for Flutter forms.
+
+
+## Getting started
+
+First, add the `field_validators` package to your pubspec.yaml.
+```yaml
+dependencies:
+  field_validators: <Package version>
+```
+or
+```yaml
+dependencies:
+  field_validators:
+    git: 
+      url: https://github.com/AxonDesigns/field_validators.git
+```
+then, execute `flutter pub get`.
+
+Now, import the package in your Dart code, use:
+```dart
+import 'package:field_validators/field_validators.dart';
+```
+
+## Usage
+
+Simply use the `FieldValidators` class to validate a `textFormField`.
+
+```dart
+TextFormField(
+  ...
+  validator: FieldValidators.required(error: "Field is required"),
+),
+```
+
+You can also use the `compose` function to combine multiple validators:
+
+```dart
+FormField<String>(
+  ...
+  validator: FieldValidators.compose([
+    FieldValidators.required(error: "Field is required"),
+    FieldValidators.number(error: "Field must be a number"),
+    FieldValidators.min(10, error: "Field must be at least 10 digits long"),
+  ]),
+),
+```
+
+You can also use the `custom` function to create your own validator and compose it with other validators:
+
+```dart
+FormField<int>(
+  ...
+  validator: FieldValidators.compose([
+    FieldValidators.required(error: "Field is required"),
+    FieldValidators.custom((value) {
+      if ( <an even more complex validation condition> ) {
+        return "Field is invalid";
+      }
+      return null;
+    }),
+  ]),
+),
+```
+
+If that's not enough, you can create your own class with some validators like so:
+
+```dart
+class MyValidators {
+  static String? Function(T? value) myValidator<T>(String error) {
+    return (value) {
+      if (<another more complex validation condition>) {
+        return error;
+      }
+      return null;
+    };
+  }
+}
+```
+
+And then use it like this:
+
+```dart
+FormField<int>(
+  ...
+  validator: FieldValidators.compose([
+    FieldValidators.required(error: "Field is required"),
+    MyValidators.myValidator("Field is invalid"),
+  ]),
+),
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ lib/field_validators.dart
@@ -0,0 +1,89 @@
+library field_validators;
+
+typedef ValidationFunction<T> = String? Function(T? value);
+
+class FieldValidators {
+  static ValidationFunction<T> compose<T>(List<ValidationFunction<T>> validators) {
+    return (value) {
+      for (var validator in validators) {
+        if (validator(value) != null) {
+          return validator(value);
+        }
+      }
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> email<T>({String error = "Invalid email"}) {
+    return (value) {
+      if (value == null) return null;
+      if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").hasMatch(value.toString())) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> required<T>({String error = "Required"}) {
+    return (value) {
+      if (value == null) return error;
+      if (value.toString().isEmpty) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> minLength<T>(int minLength, {String error = "Too short"}) {
+    return (value) {
+      if (value == null) return error;
+      if (value.toString().length < minLength) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> maxLength<T>(int maxLength, {String error = "Too long"}) {
+    return (value) {
+      if (value == null) return error;
+      if (value.toString().length > maxLength) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> exactLength<T>(int maxLength, {String error = "Invalid length"}) {
+    return (value) {
+      if (value == null) return error;
+      if (value.toString().length != maxLength) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> lengthBetween<T>(int minLength, int maxLength, {String errorShort = "Too short", String errorLong = "Too long"}) {
+    return (value) {
+      if (value == null) return errorShort;
+      if (value.toString().length < minLength) return errorShort;
+      if (value.toString().length > maxLength) return errorLong;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> number<T>({String error = "Invalid number"}) {
+    return (value) {
+      if (value == null) return error;
+      if (double.tryParse(value.toString()) == null) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> equals<T>(String other, {String error = "Invalid"}) {
+    return (value) {
+      if (value == null) return error;
+      if (value.toString() != other) return error;
+      return null;
+    };
+  }
+
+  static ValidationFunction<T> custom<T>({required ValidationFunction<T> validator, String error = "Invalid"}) {
+    return (value) {
+      if (value == null) return error;
+      if (validator(value) != null) return error;
+      return null;
+    };
+  }
+}
